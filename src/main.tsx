d23ed0a4caddcce140198a416915d647371f469e
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-01-09 18:11:24 -0300

    Intalled and implemented react router dom and edited project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
